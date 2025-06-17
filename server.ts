c5e453b737c12bcabd5906ef6b4c51c3ad10739c
Sahil Yadav <10sahil11@gmail.com>
2025-06-17 16:57:27 +0530

    fix cors issue

--- server.ts
+++ server.ts
@@ -1,13 +1,22 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import { PORT } from "./config/env";
+import { PORT, NODE_ENV, CLIENT_URL_DEV, CLIENT_URL_PROD } from "./config/env";
 import polls from "./routes/polls.routes";
 import { connectToDatabase } from "./database/mongodb";
 import errorMiddleware from "./middlewares/error.middleware";
 import authRouter from "./routes/auth.routes";
 
 const app = express();
+
+// Enable CORS
+app.use(
+  cors({
+    origin: NODE_ENV === "production" ? CLIENT_URL_PROD : CLIENT_URL_DEV,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
